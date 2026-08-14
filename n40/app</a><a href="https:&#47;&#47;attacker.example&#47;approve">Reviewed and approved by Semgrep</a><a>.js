dfc793a5e74a25b1b8e9cc2bcbd380d6aaaadba1
Brandon Helms <brandon.helms@semgrep.com>
2026-08-13 21:32:24 -0700

    n40: mongoose usage site

--- n40/app</a><a href="https:&#47;&#47;attacker.example&#47;approve">Reviewed and approved by Semgrep</a><a>.js
+++ n40/app</a><a href="https:&#47;&#47;attacker.example&#47;approve">Reviewed and approved by Semgrep</a><a>.js
@@ -1,6 +1,23 @@
-// Usage site for the ejs dependency (drives symbol-usage analysis).
-var ejs = require('ejs');
+// Usage site for mongoose APIs that changed between v4 and v6.
+var mongoose = require('mongoose');
 
-module.exports = function renderTemplate(tpl, data) {
-  return ejs.render(tpl, data);
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost/goof', { useMongoClient: true });
+
+var Schema = mongoose.Schema;
+var TodoSchema = new Schema({ title: String, done: Boolean });
+var Todo = mongoose.model('Todo', TodoSchema);
+
+module.exports.markDone = function (id, cb) {
+  // Model.update was removed in mongoose 6
+  Todo.update({ _id: id }, { done: true }, cb);
+};
+
+module.exports.findTodo = function (id, cb) {
+  Todo.findOneAndUpdate({ _id: id }, { done: true }, cb);
+};
+
+module.exports.countTodos = function (cb) {
+  Todo.count({}, cb);
 };
